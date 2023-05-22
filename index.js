034f3c431d8fa9a692a4a320484c341db3d5837b
Dariaaal <1406dariaal@gmail.com>
2023-05-22 21:19:58 +0300

    commander

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-// const argv = require("yargs").argv;
 const contacts = require("./contacts");
+const { Command } = require("commander");
+
+const program = new Command();
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
 
 const invokeAction = async ({ action, id, name, email, phone }) => {
     switch(action) {
@@ -20,34 +33,4 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
     }
 }
 
-// invokeAction({action: "list"});
-// invokeAction({action: "get", id:"qdggE76Jtbfd9eWJHrssH"});
-// invokeAction({action: "add", name: "Adam Berg", email: "adam@gmail.com", phone: "(678) 387-9902"});
-invokeAction({action: "remove", id: "CarfsK27Y6-ZS3xPc1Ggf"});
-
-
-
-// function invokeAction({ action, id, name, email, phone }) {
-//   switch (action) {
-//     case "list":
-//       // ...
-//       break;
-
-// //     case "get":
-// //       // ... id
-// //       break;
-
-// //     case "add":
-// //       // ... name email phone
-// //       break;
-
-// //     case "remove":
-// //       // ... id
-// //       break;
-
-// //     default:
-// //       console.warn("\x1B[31m Unknown action type!");
-//   }
-// }
-
-// invokeAction(argv);
+invokeAction(argv);
